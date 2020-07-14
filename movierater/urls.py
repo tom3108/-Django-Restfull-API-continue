eccb5abe94f5fa7297e4baf0ec9041300b95298c
tom3108 <t.madziara@gmail.com>
2020-07-14 19:20:01 +0200

    TokenAuthentication

--- movierater/urls.py
+++ movierater/urls.py
@@ -3,9 +3,10 @@ from api import urls
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.authtoken import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
-
-

98d7622f38191ba434992b16c6aa9a03bca4751c
tom3108 <t.madziara@gmail.com>
2020-07-16 21:51:11 +0200

    Create user

--- api/serializers.py
+++ api/serializers.py
@@ -6,8 +6,11 @@ from .models import Movie, ExtraInfo, Rate, Actor
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
-
+        fields = ['id', 'username', 'email','password']
+        extra_kwargs = {'password':{'required':True, 'write_only':True}}
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class ExtraInfoSerializer(serializers.ModelSerializer):
     class Meta:
